b1d499481b12450e0d3805e1205828c10385627a
YuliaPortnova <yuyul@yandex.ru>
2023-12-06 22:24:24 +0300

    fix: уточняет имена массивов с данными

--- js/main.js
+++ js/main.js
@@ -26,8 +26,8 @@ const createRandomLocation = () => ({
 const createRandomOffer = (currentIndex) => {
   const titles = ['Предложение 1', 'Предложение 2', 'Предложение 3', 'Предложение 4', 'Предложение 5', 'Предложение 6', 'Предложение 7', 'Предложение 8', 'Предложение 9', 'Предложение 10'];
   const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  const checkins = ['12:00', '13:00', '14:00'];
-  const checkouts = ['12:00', '13:00', '14:00'];
+  const checkinHours = ['12:00', '13:00', '14:00'];
+  const checkoutHours = ['12:00', '13:00', '14:00'];
   const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   const descriptions = ['Описание 1', 'Описание 2', 'Описание 3', 'Описание 4', 'Описание 5', 'Описание 6', 'Описание 7', 'Описание 8', 'Описание 9', 'Описание 10'];
   const photoUrls = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
@@ -39,8 +39,8 @@ const createRandomOffer = (currentIndex) => {
     type: types[getRandomInteger(0, 4)],
     rooms: getRandomInteger(1, 4),
     guests: getRandomInteger(1, 10),
-    checkin: checkins[getRandomInteger(0, 2)],
-    checkout: checkouts[getRandomInteger(0, 2)],
+    checkin: checkinHours[getRandomInteger(0, 2)],
+    checkout: checkoutHours[getRandomInteger(0, 2)],
     features: getRandomNumberOfRandomElements(features),
     description: descriptions[currentIndex],
     photos: getRandomNumberOfRandomElements(photoUrls),
@@ -58,4 +58,4 @@ const createAnnouncementsData = (announcementsCount = 10) => {
   return announcementsData;
 };
 
-console.log(createAnnouncementsData());
+void(createAnnouncementsData);
