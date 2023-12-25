9b3cf51a609f32e0699dffa719ccbd1afff81435
YuliaPortnova <yuyul@yandex.ru>
2023-12-25 22:54:03 +0300

    refactor: удаляет модул генерации случайных данных

--- js/utilities.js
+++ js/utilities.js
@@ -7,19 +7,4 @@ const request = async (url, options) => {
   return response.json();
 };
 
-const getRandomFloat = (min, max) => Math.random() * (max - min) + min;
-
-const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-const getRandomNumberOfRandomElements = (sequence) => {
-  const sequenceCopy = [...sequence];
-  const result = [];
-  while (sequenceCopy.length) {
-    const randomNumber = getRandomInteger(0, sequenceCopy.length - 1);
-    result.push(sequenceCopy.splice(randomNumber, 1)[0]);
-  }
-  const randomLength = getRandomInteger(0, sequence.length);
-  return result.splice(0, randomLength);
-};
-
-export { getRandomFloat, getRandomInteger, getRandomNumberOfRandomElements, request};
+export { request };
